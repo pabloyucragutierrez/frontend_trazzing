69b032d5dbe9404150808cc9d8cc3dd00773d45a
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-12-13 12:24:30 -0500

    cambios

--- src/app/completar-perfil/completar-perfil.component.ts
+++ src/app/completar-perfil/completar-perfil.component.ts
@@ -24,4 +24,43 @@ export class CompletarPerfilComponent {
       reader.readAsDataURL(file); 
     }
   }
+  
+  
+  fileName: string | null = null;
+  isModalVisible: boolean = false;
+  activeCard: 'card1' | 'card2' | null = null;
+
+  handleFileUpload(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      if (file.type === 'application/pdf') {
+        this.fileName = file.name;
+        this.isModalVisible = true;
+      } else {
+        alert('Por favor, sube un archivo en formato PDF.');
+        input.value = '';
+      }
+    }
+  }
+
+  triggerFileInput(): void {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    fileInput.click();
+  }
+
+  closeModal(): void {
+    this.isModalVisible = false;
+  }
+
+  showCard(card: 'card1' | 'card2'): void {
+    this.activeCard = card; // Mostrar la tarjeta seleccionada
+    this.isModalVisible = false; // Cerrar el modal principal
+  }
+
+  closeCard(): void {
+    this.activeCard = null; // Cerrar cualquier tarjeta activa
+  }
 }
