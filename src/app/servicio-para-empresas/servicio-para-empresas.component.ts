7dd9146668e2c7e3ec798399454e4e95ea62c052
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-12-16 14:46:17 -0500

    trazzing

--- src/app/servicio-para-empresas/servicio-para-empresas.component.ts
+++ src/app/servicio-para-empresas/servicio-para-empresas.component.ts
@@ -9,14 +9,16 @@ export class ServicioParaEmpresasComponent implements OnInit {
   isModalOpen: boolean = false; // Controla si el modal está visible
   isPay2ModalOpen: boolean = false; // Controla si modal__pay2 está visible
   selectedPlan: string = ''; // Almacena el título del plan seleccionado
+  selectedPlanPrice: number = 0; // Precio del plan seleccionado
+  activePlanIndex: number | null = null; // Índice del plan activo
 
   // Información de las tarjetas
   cards = [
     {
       title: 'The Coach',
       description: 'Ideal para empresas con 50 a 300 colaboradores.',
-      monthlyPrice: '$59',
-      annualPrice: '$50',
+      monthlyPrice: 59,
+      annualPrice: 50,
       features: [
         'Gestión completa del proceso de reclutamiento',
         'Acceso a base de datos de candidatos',
@@ -26,9 +28,9 @@ export class ServicioParaEmpresasComponent implements OnInit {
     },
     {
       title: 'The Manager',
-      description: 'Ideal para empresas con 50 a 300 colaboradores.',
-      monthlyPrice: '$59',
-      annualPrice: '$50',
+      description: 'Perfecto para empresas con 301 a 500 colaboradores.',
+      monthlyPrice: 179,
+      annualPrice: 150,
       features: [
         'Gestión completa del proceso de reclutamiento',
         'Acceso a base de datos de candidatos',
@@ -38,9 +40,9 @@ export class ServicioParaEmpresasComponent implements OnInit {
     },
     {
       title: 'The Boss',
-      description: 'Ideal para empresas con 50 a 300 colaboradores.',
-      monthlyPrice: '$59',
-      annualPrice: '$50',
+      description: 'Diseñado para empresas con más de 500 colaboradores.',
+      monthlyPrice: 229,
+      annualPrice: 200,
       features: [
         'Gestión completa del proceso de reclutamiento',
         'Acceso a base de datos de candidatos',
@@ -65,7 +67,15 @@ export class ServicioParaEmpresasComponent implements OnInit {
   }
 
   showPay2Modal(): void {
-    this.isModalOpen = false; // Oculta modal__pay
-    this.isPay2ModalOpen = true; // Muestra modal__pay2
+    if (this.activePlanIndex !== null) {
+      this.isModalOpen = false; // Oculta modal__pay
+      this.isPay2ModalOpen = true; // Muestra modal__pay2
+    }
+  }
+
+  setActivePlan(planIndex: number): void {
+    this.activePlanIndex = planIndex; // Almacena el índice del plan seleccionado
+    this.selectedPlan = planIndex === 0 ? 'Plan mensual' : 'Plan anual'; // Cambia el texto del plan seleccionado
+    this.selectedPlanPrice = planIndex === 0 ? 59 : 600; // Cambia el precio del plan
   }
 }
